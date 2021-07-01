d3a0f1463afe9c03bef0e7ca3c267f54a6632902
saucecodee <ashawinta@gmail.com>
2021-07-01 23:26:03 +0100

    initial setup

---
+++ src/config/index.js
@@ -0,0 +1,37 @@
+// Load env to process.env
+require("dotenv").config()
+
+
+const env = process.env
+
+module.exports = {
+   APP_NAME: env.APP_NAME,
+   MONGODB_URI: env.MONGODB_URI,
+   AUTH_JWT_SECRET: env.AUTH_JWT_SECRET,
+   BCRYPT_SALT: Number(env.BCRYPT_SALT),
+
+   "role": {
+      ADMIN: ["admin"],
+      USER: ["user", "admin"]
+   },
+
+   "user_status": {
+      SUSPENDED: "suspended",
+      ACTIVE: "active",
+      DEACTIVED: "deactived"
+   },
+
+   "url": {
+      BASE_URL: env.BASE_URL,
+      CLIENT_URL: env.CLIENT_URL
+   },
+ 
+   "mailer": {
+      HOST: env.MAILER_HOST,
+      USER: env.MAILER_USER,
+      PASSWORD: env.MAILER_PASSWORD,
+      PORT: env.MAILER_PORT,
+      SECURE: env.MAILER_PASSWORD,
+      DOMAIN: env.MAILER_DOMAIN
+   }
+}
---
+++ src/utils/response.js
@@ -0,0 +1,20 @@
+/**
+ * Returns response object
+ * @param {string} message Response message
+ * @param {*} data Data to be returned
+ * @param {boolean} success Status of the request
+ */
+
+function response(message, data, success) {
+   if (!message) message = ""
+   message = message.charAt(0).toUpperCase() + message.slice(1)
+
+   return {
+      message: message,
+      data: data || null,
+      success: success == null ? true : success
+   };
+}
+
+
+module.exports = response
